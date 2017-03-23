e3624270683be4290e79d1d8c56eeec0b682ccee
NamalD <dayarathnand@gmail.com>
2017-03-23 17:26:18 +0000

    Turtle draws a straight line when 'Run' is clicked

--- Display.pas
+++ Display.pas
@@ -5,13 +5,14 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
-  FMX.Controls.Presentation, FMX.StdCtrls;
+  FMX.Controls.Presentation, FMX.StdCtrls, Turtle;
 
 type
   TForm1 = class(TForm)
     Image1: TImage;
-    procedure Button1Click(Sender: TObject);
+    Button1: TButton;
     procedure FormCreate(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -21,14 +22,31 @@ type
 var
   Form1: TForm1;
 
+
 implementation
 
 {$R *.fmx}
 
+var
+  testTurtle: TTurtle;
+
 procedure TForm1.FormCreate(Sender: TObject);
+
+var
+  startPoint: TPointF;
 begin
   Image1.Bitmap.SetSize(Round(Image1.Width), Round(Image1.Height));
   Image1.Bitmap.Clear(TAlphaColors.White);
+
+  // Initialise turtle
+  startPoint := TPointF.Create(300, 300);
+  testTurtle := TTurtle.Create(startPoint, 20);
+end;
+
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  testTurtle.DrawLine;
 end;
 
 end.

7d5787b9a3d07c6f06a9cd17db67d709c0dd809b
NamalD <dayarathnand@gmail.com>
2017-03-24 14:36:29 +0000

    Menu bar added, allowing user to select grammar and iterations. Koch curve, Dragon curve and Sierpinski traiangle grammars defined.

--- DefinedGrammars.pas
+++ DefinedGrammars.pas
@@ -8,24 +8,49 @@ uses
 procedure InitialiseGrammars;
 
 var
-  kochCurveGrammar: TGrammar;
+  kochCurveGrammar, sierpinskiGrammar, dragonCurveGrammar: TGrammar;
 
 implementation
 
 procedure InitialiseGrammars;
 var
-  tempRules: TDictionary<String, String>;
-  tempMovements: TDictionary<String, String>;
+  kcRules, stRules, dcRules: TDictionary<String, String>;
+  kcMovements, stMovements, dcMovements: TDictionary<String, String>;
 begin
-  // Initialise Koch curve grammar rules
-  tempRules := TDictionary<String, String>.Create;
-  tempRules.Add('F', 'F+F-F-F+F');
+  // Koch curve grammar rules
+  kcRules := TDictionary<String, String>.Create;
+  kcRules.Add('F', 'F+F-F-F+F');
   // Movement rules
-  tempMovements := TDictionary<String, String>.Create;
-  tempMovements.Add('F', 'draw forward');
-  tempMovements.Add('+', 'turn 90 L');  // 90 degrees left
-  tempMovements.Add('-', 'turn 90 R');  // 90 degrees right
-  kochCurveGrammar := TGrammar.Create('F', tempRules, tempMovements);
+  kcMovements := TDictionary<String, String>.Create;
+  kcMovements.Add('F', 'draw forward');
+  kcMovements.Add('+', 'turn 90 L');  // 90 degrees left
+  kcMovements.Add('-', 'turn 90 R');  // 90 degrees right
+  kochCurveGrammar := TGrammar.Create('F', kcRules, kcMovements);
+
+  // Sierpinski triangle grammar rules
+  stRules := TDictionary<String, String>.Create;
+  stRules.Add('F', 'F-G+F+G-F');
+  stRules.Add('G', 'GG');
+  // Movement rules
+  stMovements := TDictionary<String, String>.Create;
+  stMovements.Add('F', 'draw forward');
+  stMovements.Add('G', 'draw forward');
+  stMovements.Add('+', 'turn 120 L'); // 120 degrees left
+  stMovements.Add('-', 'turn 120 R'); // 120 degrees right
+  sierpinskiGrammar := TGrammar.Create('F-G-G', stRules, stMovements);
+
+  // Dragon curve grammar rules
+  dcRules := TDictionary<String, String>.Create;
+  dcRules.Add('F', 'F-H');
+  dcRules.Add('H', 'F+H');
+  // Movement rules
+  dcMovements := TDictionary<String, String>.Create;
+  dcMovements.Add('F', 'draw forward');
+  dcMovements.Add('H', 'draw forward');
+  dcMovements.Add('+', 'turn 90 R');  // 90 degrees right
+  dcMovements.Add('-', 'turn 90 L');  // 90 degrees left
+  dragonCurveGrammar := TGrammar.Create('F', dcRules, dcMovements);
+
 end;
 
 end.
--- Display.pas
+++ Display.pas
@@ -6,20 +6,24 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
   FMX.Controls.Presentation, FMX.StdCtrls, Turtle, FMX.Edit, FMX.EditBox,
-  FMX.NumberBox, DefinedGrammars, Parser;
+  FMX.NumberBox, DefinedGrammars, Parser, FMX.Menus, FMX.ExtCtrls;
 
 type
   TForm1 = class(TForm)
     Image1: TImage;
-    Button1: TButton;
-    Button2: TButton;
-    NumberBox1: TNumberBox;
-    Button3: TButton;
-    NumberBox2: TNumberBox;
+    MenuBar1: TMenuBar;
+    Grammar: TMenuItem;
+    RunButton: TButton;
+    DragonCurve: TMenuItem;
+    SierpinskiTriangle: TMenuItem;
+    KochCurve: TMenuItem;
+    ItNumBox: TNumberBox;
+    Text1: TText;
     procedure FormCreate(Sender: TObject);
-    procedure Button1Click(Sender: TObject);
-    procedure Button2Click(Sender: TObject);
-    procedure Button3Click(Sender: TObject);
+    procedure DragonCurveClick(Sender: TObject);
+    procedure RunButtonClick(Sender: TObject);
+    procedure KochCurveClick(Sender: TObject);
+    procedure SierpinskiTriangleClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -34,45 +38,84 @@ implementation
 
 {$R *.fmx}
 
+uses
+  Grammar;
+
 var
   testTurtle: TTurtle;
+  selectedGrammar: TGrammar;
+  startPoint: TPointF;
+  drawSize: Integer;
 
 procedure TForm1.FormCreate(Sender: TObject);
-
-var
-  startPoint: TPointF;
 begin
   Image1.Bitmap.SetSize(Round(Image1.Width), Round(Image1.Height));
   Image1.Bitmap.Clear(TAlphaColors.White);
-
-  // Initialise turtle
-  startPoint := TPointF.Create(0, 200);
-  testTurtle := TTurtle.Create(startPoint, 5);
 
   // Initialise grammars
   InitialiseGrammars;
 end;
 
-// Draw a straight line using turtle, when button is clicked
-procedure TForm1.Button1Click(Sender: TObject);
+
+// Grammar menu items
+// Koch curve menu item selected
+procedure TForm1.KochCurveClick(Sender: TObject);
 begin
-  testTurtle.DrawLine;
+  selectedGrammar := kochCurveGrammar;
+  drawSize := 7;
+  startPoint := TPointF.Create(15, 300);
+
+  // Reccomended max iterations
+  ItNumBox.Max := 4;
 end;
 
-// Rotate turtle by angle in number box
-procedure TForm1.Button2Click(Sender: TObject);
+// Dragon curve menu item selected
+procedure TForm1.DragonCurveClick(Sender: TObject);
 begin
-  testTurtle.Rotate(Round(NumberBox1.Value), 'R');
+  selectedGrammar := dragonCurveGrammar;
+  drawSize := 5;
+  startPoint := TPointF.Create(425, 250);
+
+  // Recommended max iterations for canvas size
+  ItNumBox.Max := 12;
 end;
 
-// Test parser with Koch curve grammar
-procedure TForm1.Button3Click(Sender: TObject);
+// Sierpinski triangle menu item selected
+procedure TForm1.SierpinskiTriangleClick(Sender: TObject);
+begin
+  selectedGrammar := sierpinskiGrammar;
+  drawSize := 7;
+  startPoint := TPointF.Create(75, 0);
+
+  // Reccomended max iterations
+  ItNumBox.Max := 6;
+end;
+
+// When Run button clicked, draw fractal with turtle
+procedure TForm1.RunButtonClick(Sender: TObject);
 var
   production: String;
-  moveList: TStringList;
+  movements: TStringList;
 begin
-  production := Parse(kochCurveGrammar, Round(NumberBox2.Value));
-  moveList := FetchMovements(kochCurveGrammar, production);
-  testTurtle.InterpretMoves(moveList);
+  if not Assigned(selectedGrammar) then
+    ShowMessage('Please select a grammar')
+  else if ItNumBox.Value = 0 then
+    ShowMessage('Please select desired iteration')
+  else
+  begin
+    // Clear screen
+    Image1.Bitmap.Clear(TAlphaColors.White);
+
+    // Initialise turtle
+    testTurtle := TTurtle.Create(startPoint, drawSize);
+
+    // Produce production at desired iteration step
+    production := Parse(selectedGrammar, Round(ItNumBox.Value));
+    // Fetch corresponding movements using the production string
+    movements := FetchMovements(selectedGrammar, production);
+    // Instruct turtle to draw fractal by interpreting given movements
+    testTurtle.InterpretMoves(movements);
+  end;
 end;
+
 end.
